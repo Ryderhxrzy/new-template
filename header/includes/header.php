d51df9486eb68750efc955643ea01ace25e1a3be
Jhay-R Z. Cervantes <jhayrcervantes0981@gmail.com>
2025-12-07 18:16:12 +0800

    add icon in every page

--- header/includes/header.php
+++ header/includes/header.php
@@ -10,6 +10,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Template</title>
+    <link rel="icon" type="image/x-icon" href="images/favicon.ico">
     <link rel="stylesheet" href="css/global.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/sweetalert2@11/dist/sweetalert2.min.css">
